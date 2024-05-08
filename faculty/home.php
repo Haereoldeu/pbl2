37286bd44d8d95432a37e84bb75f95eed26776bc
harold vicquerra <hvicquerra@gmail.com>
2024-05-08 23:47:16 +0800

    harold

---
+++ faculty/home.php
@@ -0,0 +1,23 @@
+<?php include('db_connect.php');
+function ordinal_suffix1($num){
+    $num = $num % 100; // protect against large numbers
+    if($num < 11 || $num > 13){
+         switch($num % 10){
+            case 1: return $num.'st';
+            case 2: return $num.'nd';
+            case 3: return $num.'rd';
+        }
+    }
+    return $num.'th';
+}
+$astat = array("Not Yet Started","On-going","Closed");
+ ?>
+
+<div class="col-12">
+    <div class="card">
+      <div class="card-body">
+        Welcome <?php echo $_SESSION['login_name'] ?>!
+        <br>
+      </div>
+    </div>
+</div>
